d9eae50e88260137ffd3f578e3ebe83c07bc87bf
mlarcher531 <mlarcher531@gmail.com>
2020-10-14 14:52:25 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+const superbowlWin = (record) => {
+  const win = record.find(record => record.result == "W")
+  return win ? win.year : undefined
+}
