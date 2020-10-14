dd1ebe27f718929f3d7e115c6ca5dde8b3f2ecc8
stephenandersondev <stephenandersondev@gmail.com>
2020-10-14 16:30:40 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array) {
+ let winner = array.find(obj => obj.result == "W")
+ return winner ? winner.year : undefined
+}
