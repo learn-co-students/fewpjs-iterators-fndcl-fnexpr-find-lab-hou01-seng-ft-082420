0f1d82d4b6a8f7f05beaaafe6d745f5234ca8a9a
Billyhelm <billyhelm88@gmail.com>
2020-10-15 14:17:21 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+function superbowlWin(array){
+  let blah = array.find(function(e){return e.result === "W"})
+  return blah == undefined ? undefined : blah.year
+}
