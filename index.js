405614b3cf35b9c392257d5ee26245ff1e3e3f17
breewarren <ambreea.warren@gmail.com>
2020-10-16 21:04:49 -0500

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-const testVar = {}
+// const testVar = {}
 
-function testFunc() {
-  return "hi"
+// function testFunc() {
+//   return "hi"
+// }
+
+
+const superbowlWin = (array) => {
+let result = array.find(winOrLoss => winOrLoss.result == "W")
+return result ? result.year : undefined
 }
